bda99fba2e4d0e66625435371db08ca154b7c3b9
Anas Salha <anassalha96@gmail.com>
2024-12-04 20:39:43 +0100

    day3: Add enable/disable functionality

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -4,12 +4,23 @@ fn main() {
     let path = "data/input";
     let data = fs::read_to_string(path).unwrap();
     let data = data.replace('\n', " ");
+    let operations = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don\'t\(\)").unwrap();
 
-    let operations = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+    let mut flag = true;
 
     let result: u32 = operations
         .captures_iter(&data)
-        .map(|c| c[1].parse::<u32>().unwrap() * c[2].parse::<u32>().unwrap())
+        .map(|c| match &c[0] {
+            "do()" => {
+                flag = true;
+                return 0;
+            }
+            "don't()" => {
+                flag = false;
+                return 0;
+            }
+            _ => c[1].parse::<u32>().unwrap() * c[2].parse::<u32>().unwrap() * (flag as u32),
+        })
         .sum();
 
     dbg!(&result);
